8de406d54b4bb1e7cef5ca7d12fbfcaacf32aa15
Spencerjibz <=spencernajib2@gmail.com>
2024-05-01 17:27:55 +0100

    add tokio console

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ use lib::{
 
 #[tokio::main]
 async fn main() {
+    console_subscriber::init();
     let task1 = run_order_book::<OkexResponse>("okex", Some("BTC-USD-240429-56000-C"));
     let task = tokio::spawn(run_order_book::<DeribitResponse>("deribit", None));
-    let tasks = tokio::spawn(join(task1, task));
-
-    _ = tasks.await;
+    let tasks = tokio::spawn(task1);
 }
